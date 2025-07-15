abcebe39f6ec06576a45f9e823ea2e9611353396
Danh Trọng Trần <danhtrongtran@192.168.1.154>
2025-07-15 10:04:14 +0700

    Cập nhật - 2025-07-15 10:04:14

--- inc/inc.php
+++ inc/inc.php
@@ -24,6 +24,7 @@ require_once GIVING_THEME_DIR . '/inc/theme-setup.php';
 require_once GIVING_THEME_DIR . '/inc/flatsome-mods.php';
 require_once GIVING_THEME_DIR . '/inc/post-types.php';
 require_once GIVING_THEME_DIR . '/inc/taxonomy-meta.php';
+require_once GIVING_THEME_DIR . '/inc/acf-fields.php';
 require_once GIVING_THEME_DIR . '/inc/elements/elements.php';
 
 // Additional files can be included here as the theme grows
