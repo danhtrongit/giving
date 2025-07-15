311baeb7486519b1665097fcc39d836263ae3c99
Danh Trọng Trần <danhtrongtran@192.168.1.154>
2025-07-15 10:26:45 +0700

    Cập nhật - 2025-07-15 10:26:45

--- inc/theme-setup.php
+++ inc/theme-setup.php
@@ -26,3 +26,10 @@ function giving_enqueue_styles() {
     wp_enqueue_script('member-tabs', get_stylesheet_directory_uri() . '/assets/js/member-tabs.js', array('jquery'), '1.0.0', true);
 }
 add_action('wp_enqueue_scripts', 'giving_enqueue_styles'); 
+
+
+add_action( 'init', function () {
+    if ( function_exists( 'add_ux_builder_post_type' ) ) {
+        add_ux_builder_post_type( 'project' );
+    }
+} );
